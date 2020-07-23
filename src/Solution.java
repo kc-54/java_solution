953f3145ed6a312cdbe17ef9328d7858e463823e
stupidDummy <kelvinchandra54@yahoo.com>
2020-07-23 13:56:57 +0800

    Question A no 2, including JUnit test case for question 2, interface for swim and Duck, Chicken class

--- src/Solution.java
+++ src/Solution.java
@@ -18,6 +18,10 @@ interface canWalk{
 	public void walk();
 }
 
+interface canSwim{
+	public void swim();
+}
+
 //abstract class for super class animal
 abstract class Animal {
 	
@@ -31,9 +35,12 @@ abstract class Animal {
 	public boolean canWalk(){
 		return canWalk.class.isAssignableFrom(this.getClass());
 	}
+	public boolean canSwim(){
+		return canSwim.class.isAssignableFrom(this.getClass());
+	}
 }
 
-
+//class bird
 class Bird extends Animal implements canSing,canWalk,canFly{	
 
 	//implements all the interface
@@ -49,4 +56,40 @@ class Bird extends Animal implements canSing,canWalk,canFly{
 		System.out.println("I can Fly");
 	}
 
+}
+
+//class duck
+class Duck extends Animal implements canSing,canWalk,canSwim,canFly{
+	
+	//implements interface
+	//the sing method will say Quack, quack
+	public void sing(){
+		System.out.println("Quack, quack");
+	}
+
+	public void swim(){
+		System.out.println("I can Swim");
+	}
+
+	public void fly(){
+		System.out.println("I can Fly");
+	}
+
+	public void walk(){
+		System.out.println("I can Walk");
+	}
+
+}
+
+class Chicken extends Animal implements canSing,canWalk{
+
+	//implements interface
+	//the sing method will say Cluck, cluck
+	public void sing(){
+		System.out.println("Cluck, cluck");
+	}
+
+	public void walk(){
+		System.out.println("I can Walk");
+	}
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
@@ -14,6 +16,36 @@ public class SolutionTest {
 		assertEquals(true, invokeMethodAndReturnBool(birdie,"canFly"));
 	}
 
+	//unit test 2a
+	@Test
+	public void testDuckSound() {
+		Animal duck = new Duck();
+		String sound = invokeMethodAndReturnPrintString(duck,"sing");
+		assertEquals(true, "Quack, quack".equals(sound));
+	}
+			
+	//unit test 2b
+	@Test
+	public void testDuckSwim() {
+		Animal duck = new Duck();
+		assertEquals(true, invokeMethodAndReturnBool(duck,"canSwim"));
+	}
+
+	//unit test 2c
+	@Test
+	public void testChickenSound() {
+		Animal chicken = new Chicken();
+		String sound = invokeMethodAndReturnPrintString(chicken,"sing");
+		assertEquals(true, "Cluck, cluck".equals(sound));
+	}
+	
+	//unit test 2d
+	@Test
+	public void testChickenFly() {
+		Animal chicken = new Chicken();
+		assertEquals(false, invokeMethodAndReturnBool(chicken,"canFly"));
+	}
+	
 	//function to get boolean value of certain ability/method of an animal
 	public Boolean invokeMethodAndReturnBool(Animal animal, String method) {
 		Boolean ablePerform = null;
@@ -37,5 +69,39 @@ public class SolutionTest {
 		}
 		
 		return ablePerform;
+	}
+
+	//function to get string value of certain ability/method of an animal
+	public String invokeMethodAndReturnPrintString(Animal animal, String method) {
+		
+		//method to capture the printout from sysout and return it in string
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		PrintStream old = System.out;
+		System.setOut(ps);
+		
+		try {
+			animal.getClass().getMethod(method).invoke(animal);
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.flush();
+		System.setOut(old);
+		//regex to remove hidden characters
+		return baos.toString().replaceAll("\\p{C}", "");
 	}
 }
